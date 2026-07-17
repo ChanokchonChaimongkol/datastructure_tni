92d717371fcf2be35d54d8ef97f2a777ff3b34b5
ChanokchonChaimongkol <chain123boy@gmail.com>
2026-07-17 10:43:39 +0700

    Add files via upload

---
+++ NumberLinkedList.java
@@ -0,0 +1,25 @@
+
+public class NumberLinkedList {
+
+	public static void main(String[] args) {
+		
+		LinkedList numbers = new LinkedList();
+		
+		numbers.insert(0, 37);
+		numbers.insert(0, 7);
+		numbers.insert(0, 4);
+		numbers.insert(0, 16);
+		System.out.println("All element = "+ numbers.travesal());
+		
+		numbers.insert(2, 67);
+		System.out.println("All element = "+ numbers.travesal());
+		
+		numbers.insert(25);
+		System.out.println("All element = "+ numbers.travesal());
+		
+		numbers.remove(2);
+		System.out.println("All element = "+ numbers.travesal());
+		
+	}
+
+}
