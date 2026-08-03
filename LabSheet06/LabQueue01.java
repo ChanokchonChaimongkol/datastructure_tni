91baf6169529d6754076c3a01d561f661c96732d
ChanokchonChaimongkol <chain123boy@gmail.com>
2026-08-03 15:42:50 +0700

    Update LabQueue01.java

--- LabSheet06/LabQueue01.java
+++ LabSheet06/LabQueue01.java
@@ -5,7 +5,7 @@ public class LabQueue01 {
     public static void main(String[] args) {
         Queue<Integer> q_number = new ArrayDeque<Integer>();
 
-        // เพิ่มหมายเลขคิว 101-105 เข้าคิวด้วยการวนลูป
+        
         for (int i = 101; i <= 105; i++) {
             q_number.offer(i);
             System.out.println("Enqueue: " + i);
@@ -13,7 +13,7 @@ public class LabQueue01 {
         System.out.println("Queue => " + q_number);
         System.out.println();
 
-        // ดึงหมายเลขคิวออกไปเรื่อยๆ จนกว่าคิวจะว่าง
+       
         while (!q_number.isEmpty()) {
             int number = q_number.poll();
             System.out.println("Calling number: " + number);
